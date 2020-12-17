5927ccd5fbfbfd0e8281eea3c2efd4e28af86fba
root <root@localhost.localdomain>
2020-12-17 10:14:13 +0100

    Commit ejercicio 13 Hecho

---
+++ Practica2/ejercicio13.cc
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main(int argc, char **argv) {
+   if(argc == 2 ){
+      int fd = open(argv[1], O_RDWR | O_TRUNC);
+      if(dup2(fd, 1) == -1){
+         printf("Error: %s\n",strerror(errno));
+         return -1;
+      }
+      printf("Esto es una cadena de caracteres\n");
+      if(dup2(fd, 2) == -1){
+         printf("Error: %s\n",strerror(errno));
+         return -1;
+      }   
+      printf("Error: %s\n",strerror(1));   
+      close(fd);
+   }
+   else{
+      printf("Parametros incorrectos");
+   }
+   return 0;
+}
